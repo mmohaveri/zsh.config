2effa7d3b41790fcb71c4647a6e8859bfc4fcd7d
Mahdi Mohaveri <mmohaveri@gmail.com>
2024-05-27 12:22:33 +0200

    Add VI mode

--- key-bindings.zsh
+++ key-bindings.zsh
@@ -1,10 +1,14 @@
-# bindkey "^[[1;5C" forward-word
-# bindkey "^[[1;5D" backward-word
-# bindkey "^[OA" history-substring-search-up
-# bindkey "^[OB" history-substring-search-down
+# ===================== Load VI Mode ===========================
+# it should be set before other binds, otherwise will disable them
+bindkey -v
+export KEYTIMEOUT=1
 
 bindkey "^L" forward-word
 bindkey "^H" backward-word
 bindkey "^K" history-substring-search-up
 bindkey "^J" history-substring-search-down
 
+bindkey -M menuselect 'h' vi-backward-char
+bindkey -M menuselect 'k' vi-up-line-or-history
+bindkey -M menuselect 'l' vi-forward-char
+bindkey -M menuselect 'j' vi-down-line-or-history
