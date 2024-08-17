fb7488d9ffae68155972f5fc022f8cc01ca21780
Mahdi Mohaveri <mmohaveri@gmail.com>
2024-08-17 16:52:13 +0200

    Only register brew autocompletion if on Mac and brew exists

--- plugins/brew/brew.plugin.zsh
+++ plugins/brew/brew.plugin.zsh
@@ -1,1 +1,3 @@
-FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
+if [ `uname -s` = "Darwin" ] && [ which brew > /dev/null 2>&1 ]; then
+  FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
+fi
