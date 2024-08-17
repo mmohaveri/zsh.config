2ac492dcadc8151e0daf4898a6138d4b90bb8bc6
Mahdi Mohaveri <mmohaveri@gmail.com>
2024-08-17 16:46:37 +0200

    Add a native-system-upgrade command if nala does not exist

--- aliases.zsh
+++ aliases.zsh
@@ -4,9 +4,22 @@ alias note="cd $HOME/Documents/Notes"
 
 # ===================== alias for frequently used commands =====
 
-# Upgrades system's packages while leaving the packages with alpha, beta, or rc upgrades left alone.
-alias system-upgrade="sudo nala update && sudo nala upgrade --no-update \$(apt-get --just-print dist-upgrade | grep 'Inst' | grep -E '\\(.*(alpha|a[0-9]|beta|b[0-9]|rc).*\\)' | cut -d' ' -f2 | xargs -I{} echo '--exclude {}')"
-
+# set an alias that upgrades system's packages while leaving the packages with alpha, beta, or rc upgrades left alone.
+if [ `uname -s` = "Linux" ]; then
+  # If nala exists
+  if which nala > /dev/null 2>&1; then
+    alias system-upgrade="sudo nala update && sudo nala upgrade --no-update \$(apt-get --just-print dist-upgrade | grep 'Inst' | grep -E '\\(.*(alpha|a[0-9]|beta|b[0-9]|rc).*\\)' | cut -d' ' -f2 | xargs -I{} echo '--exclude {}')"
+  elif which apt > /dev/null 2>&1; then
+    # Upgrades system's packages while leaving the packages with alpha, beta, or rc upgrades left alone (using apt instead of nala)
+    native-system-upgrade() {
+      sudo apt update
+      sudo apt-get --just-print dist-upgrade | grep 'Inst' | grep -E '\(.*(alpha|a[0-9]|beta|b[0-9]|rc).*\)' | cut -d " " -f2 | xargs sudo apt-mark hold | cut -d" " -f1 > /tmp/marked-as-hold.tx
+      sudo apt dist-upgrade
+      cat /tmp/marked-as-hold.txt | xargs sudo apt-mark unhold
+    }
+    echo "Install `nala` to have a better system upgrade experience."
+  fi
+fi
 
 # ls alias for colorful lists in linux
 if [ `uname -s` = "Linux" ]; then
