f63542a6f8c9a956e28e62808af6b09d500b7450
Mahdi Mohaveri <mmohaveri@gmail.com>
2024-10-24 18:05:58 +0200

    As completion generation takes a lot of time, move it to background

--- configs/9-update-completions.zsh
+++ configs/9-update-completions.zsh
@@ -1,29 +1,35 @@
-# complete -C `which aws_completer` aws
+function update-completions {
+    # AWS
+    # complete -C `which aws_completer` aws
 
-# Hatch
-_HATCH_COMPLETE=zsh_source hatch >| $ZSH_COMPLETION_FUNCTIONS/_hatch
+    # Hatch
+    _HATCH_COMPLETE=zsh_source hatch >| $ZSH_COMPLETION_FUNCTIONS/_hatch
 
-# Podman
-podman completion zsh -f "${ZSH_COMPLETION_FUNCTIONS}/_podman"
+    # Podman
+    podman completion zsh -f "${ZSH_COMPLETION_FUNCTIONS}/_podman"
 
-# Poetry (desabled)
-# poetry completions zsh >| "${ZSH_COMPLETION_FUNCTIONS}/_poetry" &|
+    # Poetry (desabled)
+    # poetry completions zsh >| "${ZSH_COMPLETION_FUNCTIONS}/_poetry" &|
 
-# Rust (rustup & cargo)
-rustup completions zsh >| "$ZSH_COMPLETION_FUNCTIONS/_rustup" &|
-cp "$(rustc +${${(z)$(rustup default)}[1]} --print sysroot)"/share/zsh/site-functions/_cargo $ZSH_COMPLETION_FUNCTIONS/_cargo
+    # Rust (rustup & cargo)
+    rustup completions zsh >| "$ZSH_COMPLETION_FUNCTIONS/_rustup" &|
+    cp "$(rustc +${${(z)$(rustup default)}[1]} --print sysroot)"/share/zsh/site-functions/_cargo $ZSH_COMPLETION_FUNCTIONS/_cargo
 
-# Kubectl
-kubectl completion zsh >| ${ZSH_COMPLETION_FUNCTIONS}/_kubectl
+    # Kubectl
+    kubectl completion zsh >| ${ZSH_COMPLETION_FUNCTIONS}/_kubectl
 
-# Brew (only on MacOS)
-if [ `uname -s` = "Darwin" ] && [ which brew > /dev/null 2>&1 ]; then
-    FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
-fi
+    # Brew (only on MacOS)
+    if [ `uname -s` = "Darwin" ] && [ which brew > /dev/null 2>&1 ]; then
+        FPATH="$(brew --prefix)/share/zsh/site-functions:${FPATH}"
+    fi
 
-# NVM
-if [ "`uname -s`" = "Darwin" ]; then
-    source /opt/homebrew/opt/nvm/etc/bash_completion.d/nvm
-else
-    source $NVM_DIR/bash_completion
-fi
+    # NVM
+    if [ "`uname -s`" = "Darwin" ]; then
+        source /opt/homebrew/opt/nvm/etc/bash_completion.d/nvm
+    else
+        source $NVM_DIR/bash_completion
+    fi
+}
+
+setopt LOCAL_OPTIONS NO_MONITOR
+update-completions &
