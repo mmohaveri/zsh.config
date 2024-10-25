67a4566698799e091330feb5b130319d67f5642c
Mahdi Mohaveri <mmohaveri@gmail.com>
2024-10-25 06:38:38 +0200

    Make prompt into right & left

--- configs/2-prompt.zsh
+++ configs/2-prompt.zsh
@@ -1,12 +1,20 @@
 SPACESHIP_PROMPT_ORDER=(
   dir            # Current directory section
-  git            # Git section (git_branch + git_status)
-  venv           # virtualenv section
-  jobs           # Background jobs indicator
   exit_code      # Exit code section
   sudo           # Sudo indicator
   char           # Prompt character
 )
+
+SPACESHIP_RPROMPT_ORDER=(
+  git          # Git information
+  node          # Node.js version from nvm
+  venv   # Python virtual environment
+  python
+  golang
+  rust
+  jobs         # Background jobs
+)
+
 
 SPACESHIP_USER_SHOW=true
 SPACESHIP_PROMPT_ADD_NEWLINE=false
@@ -15,5 +23,8 @@ SPACESHIP_CHAR_SYMBOL_ROOT="#"
 SPACESHIP_CHAR_SYMBOL="❯"
 SPACESHIP_CHAR_PREFIX=""
 SPACESHIP_CHAR_SUFFIX=" "
+SPACESHIP_EXIT_CODE_SHOW=true
+SPACESHIP_DIR_TRUNC=3
+
 
 source $ZSH/prompts/spaceship/spaceship.zsh
